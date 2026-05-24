494d0cd4bff78f829b076f7ef9c4538393848b31
DinisSL <dinissantanalopes@gmail.com>
2026-05-24 21:22:25 +0100

    breadcrumb.jsx apagado

--- frontend/src/components/CreateRace.jsx
+++ frontend/src/components/CreateRace.jsx
@@ -1,4 +1,3 @@
-// src/components/CreateRace.jsx
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
--- frontend/src/components/Layout/Layout.jsx
+++ frontend/src/components/Layout/Layout.jsx
@@ -1,17 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
-import BreadCrumb from "./BreadCrumb.jsx";
 
 const Layout = () => {
   return (
     <div className="min-h-screen flex flex-col">
-      <Header />
-      {/*<BreadCrumb />*/}
+      <Header/>
       <main className="grow flex flex-col">
-        <Outlet />
+        <Outlet/>
       </main>
-      <Footer />
+      <Footer/>
     </div>
   );
 };
